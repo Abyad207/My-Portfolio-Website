f85373dbe6880f1f6a50bda1660ec5b05b1ed825
Abyad207 <abyadkhan207@gmail.com>
2025-04-25 15:10:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -105,24 +105,4 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-<script>
-  function openModal(id) {
-    document.getElementById(id).style.display = 'block';
-  }
-
-  function closeModal(id) {
-    document.getElementById(id).style.display = 'none';
-  }
-
-  window.onclick = function (event) {
-    const modals = document.querySelectorAll('.modal');
-    modals.forEach(modal => {
-      if (event.target === modal) {
-        modal.style.display = "none";
-      }
-    });
-  };
-</script>
-
-
 
